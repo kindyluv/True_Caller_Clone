15bc6e19e3c34b765e340a85fdd44b9d9c5a32e5
precious <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>
2022-03-24 18:18:41 +0100

    ..

---
+++ src/main/java/com/precious/truecaller/web/exception/contactException/ContactAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.precious.truecaller.web.exception.contactException;
+
+public class ContactAlreadyExistsException extends RuntimeException {
+    public ContactAlreadyExistsException(String message) {
+        super(message);
+    }
+}
