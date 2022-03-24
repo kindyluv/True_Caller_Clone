7dcdba671c217e15de36bbbac09e0220ebaa851d
precious <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>
2022-03-24 18:18:34 +0100

    ..

---
+++ src/main/java/com/precious/truecaller/web/controller/ContactController.java
@@ -0,0 +1,72 @@
+package com.precious.truecaller.web.controller;
+
+
+import com.github.fge.jsonpatch.JsonPatch;
+import com.precious.truecaller.data.dto.request.ContactRequest;
+import com.precious.truecaller.data.dto.response.ContactResponse;
+import com.precious.truecaller.data.models.mobile.MobileNumber;
+import com.precious.truecaller.service.contactService.ContactService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/contact")
+public class ContactController {
+    private final ContactService contactService;
+
+    @Autowired
+    public ContactController(ContactService contactService) {
+        this.contactService = contactService;
+    }
+
+    @PostMapping
+    public ContactResponse addContact(@RequestBody ContactRequest request){
+        return contactService.addContact(request);
+    }
+
+    @GetMapping(path = "/{mobileNumber}")
+    public ContactResponse findByMobileNumber(@PathVariable @RequestBody MobileNumber mobileNumber){
+        return contactService.findContactByMobileNumber(mobileNumber);
+    }
+
+    @GetMapping(path = "/{contactName}")
+    public ContactResponse findByContactName(@PathVariable String contactName){
+        return contactService.findByContactName(contactName);
+    }
+
+    @GetMapping
+    public List<ContactResponse> findAllContact(){
+        return contactService.findAllContact();
+    }
+
+    @PatchMapping(path = "/blockContact/{mobileNumber}")
+    public Boolean blockContactByMobileNumber(@PathVariable MobileNumber mobileNumber){
+        return contactService.blockContactByMobileNumber(mobileNumber);
+    }
+    @PatchMapping(path = "/{contactName}", consumes = "application/json-patch+json")
+    public ContactResponse editContact(@PathVariable String contactName, @RequestBody JsonPatch jsonPatch){
+        return  contactService.editContact(contactName, jsonPatch);
+    }
+
+    @PatchMapping(path = "/unblockContact/{mobileNumber}")
+    public Boolean unBlockContactByMobileNumber(@PathVariable MobileNumber mobileNumber){
+        return contactService.unBlockContactByMobileNumber(mobileNumber);
+    }
+
+    @DeleteMapping(path = "/{contactName}")
+    public String deleteContactByContactName(@PathVariable String contactName){
+        return contactService.deleteContactByContactName(contactName);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public String deleteContactById(@PathVariable Integer id){
+        return contactService.deleteContactById(id);
+    }
+
+    @DeleteMapping
+    public String deleteAllContact(){
+        return contactService.deleteAllContact();
+    }
+}
