b3b0b93c42e7f4468cbb404ed0c6a77a4f548ee7
precious <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>
2022-03-24 18:18:20 +0100

    ..

---
+++ src/main/java/com/precious/truecaller/web/exception/contactException/ContactNotFoundExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.precious.truecaller.web.exception.contactException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+public class ContactNotFoundExceptionHandler {
+
+    public ResponseEntity<Object> handleContactNotFoundRequestException(ContactNotFoundException exception){
+        ContactException ex = new ContactException(
+                exception.getMessage(),
+                HttpStatus.NOT_FOUND,
+                ZonedDateTime.now(ZoneId.of("Z"))
+        );
+        return new ResponseEntity<>(ex, HttpStatus.NOT_FOUND);
+    }
+}
