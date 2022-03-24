c9082e31422e32319c76cd1430ab2a3b486cc175
precious <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>
2022-03-24 18:16:42 +0100

    ..

---
+++ src/main/java/com/precious/truecaller/web/exception/UpdateExceptionsException.java
@@ -0,0 +1,7 @@
+package com.precious.truecaller.web.exception;
+
+public class UpdateExceptionsException extends RuntimeException {
+    public UpdateExceptionsException(String message) {
+        super(message);
+    }
+}
