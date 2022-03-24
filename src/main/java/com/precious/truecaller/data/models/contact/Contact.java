382c0cfe001fe23c0b5e2421b87d346f0dc925b3
precious <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>
2022-03-24 18:18:49 +0100

    ..

---
+++ src/main/java/com/precious/truecaller/data/models/contact/Contact.java
@@ -0,0 +1,25 @@
+package com.precious.truecaller.data.models.contact;
+
+import com.precious.truecaller.data.models.mobile.MobileNumber;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String companyName;
+    @Column(unique = true)
+    private String email;
+    @OneToOne
+    private MobileNumber mobileNumber;
+    private Boolean isBlocked = false;
+}
