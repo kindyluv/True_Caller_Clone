2b8db2055c38b1bc2add4abfa0467457d8dc3c29
precious <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>
2022-03-24 18:18:15 +0100

    ..

---
+++ src/main/java/com/precious/truecaller/data/repositories/ContactRepository.java
@@ -0,0 +1,20 @@
+package com.precious.truecaller.data.repositories;
+
+import com.precious.truecaller.data.models.contact.Contact;
+import com.precious.truecaller.data.models.mobile.MobileNumber;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Repository
+@Transactional
+public interface ContactRepository extends JpaRepository<Contact, Integer> {
+   Optional<Contact> findByName(String contactName);
+
+    void deleteByName(String name);
+
+
+    Optional<Contact> findByMobileNumber(MobileNumber mobileNumber);
+}
