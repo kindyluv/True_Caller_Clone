064ff8938ed9f048d38f034ad75315f95432e7e1
precious <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>
2022-03-24 18:25:08 +0100

    ..

--- src/main/java/com/precious/truecaller/data/dto/response/MessageResponse.java
+++ src/main/java/com/precious/truecaller/data/dto/response/MessageResponse.java
@@ -1,10 +1,12 @@
 package com.precious.truecaller.data.dto.response;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Setter
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MessageResponse {
     private String messageBody;
 }
