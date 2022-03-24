d34a3b2a6a4f4ddc409c598d24f37e63b59ec023
precious <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>
2022-03-24 18:16:47 +0100

    ..

---
+++ src/main/java/com/precious/truecaller/data/models/mobile/MobileNumber.java
@@ -0,0 +1,22 @@
+package com.precious.truecaller.data.models.mobile;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Setter
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MobileNumber {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String countryCode;
+    private String mobileNumber;
+}
