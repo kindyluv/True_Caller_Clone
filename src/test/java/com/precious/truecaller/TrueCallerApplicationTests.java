a29df31a1e7cd237c593bcdc07ba3140d22a2ccd
precious <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>
2022-03-24 18:16:37 +0100

    ..

---
+++ src/test/java/com/precious/truecaller/TrueCallerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.precious.truecaller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrueCallerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
