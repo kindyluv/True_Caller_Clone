010e80f7b6bc44dfc87c8d6f44a97f6157114158
precious <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>
2022-03-24 18:25:03 +0100

    ..

--- src/main/java/com/precious/truecaller/data/dto/request/MessageRequest.java
+++ src/main/java/com/precious/truecaller/data/dto/request/MessageRequest.java
@@ -1,10 +1,12 @@
 package com.precious.truecaller.data.dto.request;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Setter
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MessageRequest {
     private String messageBody;
     private String mobileNumber;
