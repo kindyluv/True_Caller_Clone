be3bf5168585d1fdbcbb50124c392c9107129754
precious <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>
2022-03-24 18:17:20 +0100

    ..

---
+++ src/main/java/com/precious/truecaller/data/dto/response/MessageResponse.java
@@ -0,0 +1,10 @@
+package com.precious.truecaller.data.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class MessageResponse {
+    private String messageBody;
+}
