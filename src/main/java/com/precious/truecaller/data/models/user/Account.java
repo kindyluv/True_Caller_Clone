b3d0785fe9176339bd31972988419044e67d3d45
precious <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>
2022-03-25 11:06:25 +0100

    ..

--- src/main/java/com/precious/truecaller/data/models/user/Account.java
+++ src/main/java/com/precious/truecaller/data/models/user/Account.java
@@ -1,8 +1,7 @@
 package com.precious.truecaller.data.models.user;
 
 import com.precious.truecaller.data.models.contact.Contact;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -11,12 +10,17 @@ import java.util.List;
 @Setter
 @Getter
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "accounts")
 public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    @Column(unique = true)
     private String userName;
+    @Column(unique = true)
     private String email;
     private String password;
     private LocalDateTime lastAccessed;
