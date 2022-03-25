f0419131d6cdbb5d7bd85524410724c003c8c1ca
precious <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>
2022-03-25 17:20:35 +0100

    ..

--- src/main/java/com/precious/truecaller/data/repositories/MobileNumberRepository.java
+++ src/main/java/com/precious/truecaller/data/repositories/MobileNumberRepository.java
@@ -4,6 +4,9 @@ import com.precious.truecaller.data.models.mobile.MobileNumber;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MobileNumberRepository extends JpaRepository<MobileNumber, Integer> {
+    Optional<MobileNumber> findByMobileNumber(String mobileNumber);
 }
