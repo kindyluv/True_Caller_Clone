d17cb178e36765de8338bb93db31514712b2d154
precious <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>
2022-03-25 11:08:37 +0100

    ..

--- src/main/java/com/precious/truecaller/data/dto/response/ContactResponse.java
+++ src/main/java/com/precious/truecaller/data/dto/response/ContactResponse.java
@@ -3,7 +3,11 @@ package com.precious.truecaller.data.dto.response;
 import com.precious.truecaller.data.models.mobile.MobileNumber;
 import lombok.*;
 
-@Data
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ContactResponse {
     private String name;
     private MobileNumber mobileNumber;
