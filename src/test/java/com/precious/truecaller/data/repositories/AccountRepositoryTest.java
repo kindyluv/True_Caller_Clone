889074a2a6a60a6843c7133a750ffdbe7f0ccbd8
precious <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>
2022-03-25 17:19:05 +0100

    ..

---
+++ src/test/java/com/precious/truecaller/data/repositories/AccountRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.precious.truecaller.data.repositories;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
