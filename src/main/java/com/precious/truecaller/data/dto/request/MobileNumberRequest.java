dcd3bbbee5cc0aa89717b5744ac9ab6b6e3005bd
precious <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>
2022-03-25 17:20:43 +0100

    ..

--- src/main/java/com/precious/truecaller/data/dto/request/MobileNumberRequest.java
+++ src/main/java/com/precious/truecaller/data/dto/request/MobileNumberRequest.java
@@ -1,4 +1,17 @@
 package com.precious.truecaller.data.dto.request;
 
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+
+@Setter
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MobileNumberRequest {
+    @NotNull(message = "Field can not be null")
+    private String countryCode;
+    @NotNull(message = "Field can not be null")
+    private String mobileNumber;
 }
